6b81f2de93ad88fa3b013a9b63c454cda6b8f6ea
Madeleine Lorås <madeleine.loras@gmail.com>
2021-09-22 20:09:13 +0200

    start sanity setup

--- site/src/App.js
+++ site/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const client = sanityClient({
+    projectId: "33hgh7pg",
+    dataset: "production",
+    apiVersion: "2021-09-22", // use current UTC date - see "specifying API version"!
+    useCdn: true, // `false` if you want to ensure fresh data
+  });
+
   return <div className="App">Hello world</div>;
-}
+};
 
 export default App;
