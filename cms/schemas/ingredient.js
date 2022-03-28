4106fcb8f6c29d45e68eef3882b50e28c0efbeef
Madeleine Lorås <madeleine.loras@gmail.com>
2022-03-28 21:20:05 +0200

    Added cupconverter field to CMS

--- cms/schemas/ingredient.js
+++ cms/schemas/ingredient.js
@@ -9,6 +9,12 @@ export default {
       type: "string",
       description: "Name of ingredient",
     },
+    {
+      name: "cupconverter",
+      title: "Cup converter",
+      type: "number",
+      description: "Hvor mange gram tilsvarer 1 cup",
+    },
   ],
   preview: {
     select: { title: "name" },
